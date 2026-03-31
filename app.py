49f1efe82ed4b3e99b768807976befb83eea5be1
supreeth1311 <supreeth1311@gmail.com>
2026-03-31 20:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=st.secrets["gemini"]["API_KEY"])
 # -----------------------
 # Load Model
 # -----------------------
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
 # -----------------------
 # UI
