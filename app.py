9428df239d288f5fb9f9d24411b1e7a8098b963b
supreeth1311 <supreeth1311@gmail.com>
2026-03-31 20:44:38 +0530

    Update model path for GenerativeModel initialization

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=st.secrets["gemini"]["API_KEY"])
 # -----------------------
 # Load Model
 # -----------------------
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 # -----------------------
 # UI
